c16e62330b86f826429309ed7ea5d2cec50d55c8
Martin Ronning <ronningmartin@outlook.com>
2025-02-07 14:37:51 +0100

    draw cube on top of detected board

--- main.py
+++ main.py
@@ -39,4 +39,52 @@ for fname in images:
     cv.drawChessboardCorners(img, (7,7), corners2, ret)
     cv.imshow('img', img)
     cv.waitKey(500)
+  else:
+    print("not found")
+
 cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+# Function to draw a cube on the image
+def draw_cube(img, imgpts):
+    imgpts = np.int32(imgpts).reshape(-1, 2)
+    # Draw base in green.
+    img = cv.drawContours(img, [imgpts[:4]], -1, (0, 255, 0), 3)
+    # Draw vertical edges in blue.
+    for i, j in zip(range(4), range(4, 8)):
+        img = cv.line(img, tuple(imgpts[i]), tuple(imgpts[j]), (255, 0, 0), 3)
+    # Draw top in red.
+    img = cv.drawContours(img, [imgpts[4:]], -1, (0, 0, 255), 3)
+    return img
+
+# If calibration succeeded and we have at least one chessboard pose, project and draw the cube.
+if len(rvecs) > 0:
+    # Define the 3D cube points (origin at the chessboard corner).
+    cube_points = np.float32([
+        [0, 0, 0],
+        [2, 0, 0],
+        [2, 2, 0],
+        [0, 2, 0],
+        [0, 0, -2],
+        [2, 0, -2],
+        [2, 2, -2],
+        [0, 2, -2]
+    ])
+    
+    # Re-read the first image to draw the cube.
+    img = cv.imread(images[0])
+    img = cv.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
+
+    # Draw the chessboard corners on the image (using the first detected corners).
+    img = cv.drawChessboardCorners(img, (7,7), imgpoints[0], True)
+    
+    # Use the first pose (rvec and tvec) from calibration.
+    imgpts, _ = cv.projectPoints(cube_points, rvecs[0], tvecs[0], mtx, dist)
+    
+    # Draw the cube on the image.
+    img = draw_cube(img, imgpts)
+    
+    cv.imshow('Cube Projection', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
